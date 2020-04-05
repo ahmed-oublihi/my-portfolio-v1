ff77c762c3a029264779042329de08f2d57d1b34
Ahmed Oublihi <oublihi.a@gmail.com>
2020-04-05 21:14:04 +0200

    Add to gh-pages

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $('.sidenav').sidenav();
+  $('.carousel.carousel-slider').carousel({ 
+    fullWidth: true 
+  });
+  $('.button-collapse').sideNav();
+  $('.modal').modal();
+});
+
+var typed = new Typed('.typedIlement', {
+  strings: ['Web Design', 'Web Development', 'MERN-Stack'],
+  smartBackspace: true,
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 500,
+  startDelay: 1000,
+  loop: true,
+  showCursor: false,
+});
+
+particlesJS.load('particles-js', './../particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
